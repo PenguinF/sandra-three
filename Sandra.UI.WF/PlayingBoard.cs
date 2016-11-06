450136e269d7691738fa84b5c7d39b7f7ca9a13a
Henk Nicolai <hnicolai@thinkwisesoftware.com>
2016-11-06 20:36:17 +0100

    Introduced a PlayingBoard control.

---
+++ Sandra.UI.WF/PlayingBoard.cs
@@ -0,0 +1,38 @@
+﻿/*********************************************************************************
+ * PlayingBoard.cs
+ * 
+ * Copyright (c) 2004-2016 Henk Nicolai
+ * 
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ * 
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ * 
+ *********************************************************************************/
+using System.Windows.Forms;
+
+namespace Sandra.UI.WF
+{
+    public class PlayingBoard
+        : Control
+    {
+        public PlayingBoard()
+        {
+            SetStyle(ControlStyles.AllPaintingInWmPaint
+                | ControlStyles.UserPaint
+                | ControlStyles.UserMouse
+                | ControlStyles.Selectable
+                | ControlStyles.OptimizedDoubleBuffer
+                | ControlStyles.FixedHeight
+                | ControlStyles.FixedWidth
+                | ControlStyles.Opaque, true);
+        }
+    }
+}
