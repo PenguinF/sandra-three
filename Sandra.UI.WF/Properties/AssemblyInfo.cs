9cc6548e573986e9a3761fa9439d54f9fc2bc2b9
Henk Nicolai <hnicolai@thinkwisesoftware.com>
2016-11-06 18:02:48 +0100

    Added Sandra.sln and empty Windows Forms project.

---
+++ Sandra.UI.WF/Properties/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sandra")]
+[assembly: AssemblyDescription("Hosted on https://github.com/PenguinF/sandra-three")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Sandra")]
+[assembly: AssemblyCopyright("Copyright © 2002-2016 - Henk Nicolai")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
