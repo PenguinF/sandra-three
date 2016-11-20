b7b6eaed5ce548553a73980f9d376e469fe1c3ee
Henk Nicolai <hnicolai@thinkwisesoftware.com>
2016-11-20 15:25:08 +0100

    Removed Square.None.

--- Sandra.Chess/ChessTypes.cs
+++ Sandra.Chess/ChessTypes.cs
@@ -82,8 +82,6 @@ namespace Sandra.Chess
     /// </summary>
     public enum Square
     {
-        None = -1,
-
         A1, B1, C1, D1, E1, F1, G1, H1,
         A2, B2, C2, D2, E2, F2, G2, H2,
         A3, B3, C3, D3, E3, F3, G3, H3,
