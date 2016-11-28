6e4bed9c0057c3650ff5644aab8741fa070ddffb
Henk Nicolai <hnicolai@thinkwisesoftware.com>
2016-11-28 21:03:34 +0100

    Added MovesTextBox class to display a list of moves.

---
+++ Sandra.UI.WF/MovesTextBox.cs
@@ -0,0 +1,39 @@
+﻿/*********************************************************************************
+ * MovesTextBox.cs
+ * 
+ * Copyright (c) 2004-2016 Henk Nicolai
+ * 
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ * 
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ * 
+ *********************************************************************************/
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Sandra.UI.WF
+{
+    /// <summary>
+    /// Represents a read-only Windows rich text box which displays a list of chess moves.
+    /// </summary>
+    public class MovesTextBox : RichTextBox
+    {
+        public MovesTextBox()
+        {
+            ReadOnly = true;
+            BorderStyle = BorderStyle.None;
+            BackColor = Color.White;
+            ForeColor = Color.Black;
+            Font = new Font("Candara", 10);
+            AutoWordSelection = true;
+        }
+    }
+}
