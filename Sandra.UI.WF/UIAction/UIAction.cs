be8ba0940adf6577cab4160da9c51afebcff09e3
Henk Nicolai <hnicolai@thinkwisesoftware.com>
2017-02-11 22:39:52 +0100

    Added UIAction class, with an inner string key.

--- Sandra.UI.WF/UIAction/UIAction.cs
+++ Sandra.UI.WF/UIAction/UIAction.cs
@@ -16,6 +16,53 @@
  *    limitations under the License.
  * 
  *********************************************************************************/
+using System;
+
 namespace Sandra.UI.WF
 {
+    /// <summary>
+    /// Represents an immutable, opaque atom for a user action.
+    /// </summary>
+    public sealed class UIAction : IEquatable<UIAction>
+    {
+        private readonly string Key;
+
+        /// <summary>
+        /// Constructs a new instance of <see cref="UIAction"/>, in which the provided string key is used for equality comparison and hashcode generation.
+        /// </summary>
+        public UIAction(string key)
+        {
+            if (key == null) throw new ArgumentNullException(nameof(key));
+            Key = key;
+        }
+
+        public bool Equals(UIAction other)
+        {
+            return other != null && Key == other.Key;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return Equals(obj as UIAction);
+        }
+
+        public override int GetHashCode()
+        {
+            return Key.GetHashCode();
+        }
+
+        public static bool operator ==(UIAction first, UIAction second)
+        {
+            if (ReferenceEquals(null, first)) return ReferenceEquals(null, second);
+            if (ReferenceEquals(null, second)) return false;
+            return first.Key == second.Key;
+        }
+
+        public static bool operator !=(UIAction first, UIAction second)
+        {
+            if (ReferenceEquals(null, first)) return !ReferenceEquals(null, second);
+            if (ReferenceEquals(null, second)) return true;
+            return first.Key != second.Key;
+        }
+    }
 }
