0ad58e6f8dbe98c034a282bc0356bc3bd5c778d5
Henk Nicolai <hnicolai@thinkwisesoftware.com>
2017-07-02 00:11:35 +0200

    Call List<>.GetEnumerator() directly for implementing UIActionBindings.GetEnumerator().

--- Sandra.UI.WF/UIAction/UIActionBinding.cs
+++ Sandra.UI.WF/UIAction/UIActionBinding.cs
@@ -108,9 +108,7 @@ namespace Sandra.UI.WF
 
         public void Add(DefaultUIActionBinding key, UIActionHandlerFunc value) => added.Add(new BindingHandlerPair(key, value));
 
-        IEnumerator<BindingHandlerPair> enumerate() { foreach (var x in added) yield return x; }
-
-        IEnumerator IEnumerable.GetEnumerator() => enumerate();
-        IEnumerator<BindingHandlerPair> IEnumerable<BindingHandlerPair>.GetEnumerator() => enumerate();
+        IEnumerator IEnumerable.GetEnumerator() => added.GetEnumerator();
+        IEnumerator<BindingHandlerPair> IEnumerable<BindingHandlerPair>.GetEnumerator() => added.GetEnumerator();
     }
 }
