7f12ba0f8d84ebb9726d064ea0f50a493181259c
Henk Nicolai <hnicolai@thinkwisesoftware.com>
2018-01-08 23:33:51 +0100

    Separated PGNTerminalSymbol data from the code acting on it by adding a visitor pattern.

--- Sandra.Chess/PGN/PGNSyntax.cs
+++ Sandra.Chess/PGN/PGNSyntax.cs
@@ -22,55 +22,99 @@ namespace Sandra.PGN
 {
     public interface PGNTerminalSymbol : IEquatable<PGNTerminalSymbol>
     {
+        void Accept(PGNTerminalSymbolVisitor visitor);
+        TResult Accept<TResult>(PGNTerminalSymbolVisitor<TResult> visitor);
         string GetText();
+    }
+
+    public abstract class PGNTerminalSymbolVisitor
+    {
+        public virtual void DefaultVisit(PGNTerminalSymbol symbol) { }
+        public virtual void Visit(PGNTerminalSymbol symbol) { if (symbol != null) symbol.Accept(this); }
+        public virtual void VisitBlackToMoveEllipsisSymbol(BlackToMoveEllipsisSymbol symbol) => DefaultVisit(symbol);
+        public virtual void VisitFormattedMoveSymbol(FormattedMoveSymbol symbol) => DefaultVisit(symbol);
+        public virtual void VisitMoveCounterSymbol(MoveCounterSymbol symbol) => DefaultVisit(symbol);
+        public virtual void VisitSideLineEndSymbol(SideLineEndSymbol symbol) => DefaultVisit(symbol);
+        public virtual void VisitSideLineStartSymbol(SideLineStartSymbol symbol) => DefaultVisit(symbol);
+        public virtual void VisitSpaceSymbol(SpaceSymbol symbol) => DefaultVisit(symbol);
+    }
+
+    public abstract class PGNTerminalSymbolVisitor<TResult>
+    {
+        public virtual TResult DefaultVisit(PGNTerminalSymbol symbol) => default(TResult);
+        public virtual TResult Visit(PGNTerminalSymbol symbol) => symbol == null ? default(TResult) : symbol.Accept(this);
+        public virtual TResult VisitBlackToMoveEllipsisSymbol(BlackToMoveEllipsisSymbol symbol) => DefaultVisit(symbol);
+        public virtual TResult VisitFormattedMoveSymbol(FormattedMoveSymbol symbol) => DefaultVisit(symbol);
+        public virtual TResult VisitMoveCounterSymbol(MoveCounterSymbol symbol) => DefaultVisit(symbol);
+        public virtual TResult VisitSideLineEndSymbol(SideLineEndSymbol symbol) => DefaultVisit(symbol);
+        public virtual TResult VisitSideLineStartSymbol(SideLineStartSymbol symbol) => DefaultVisit(symbol);
+        public virtual TResult VisitSpaceSymbol(SpaceSymbol symbol) => DefaultVisit(symbol);
     }
 
     public sealed class SpaceSymbol : PGNTerminalSymbol
     {
         public const string SpaceText = " ";
+
         public bool Equals(PGNTerminalSymbol other) => other is SpaceSymbol;
+        public void Accept(PGNTerminalSymbolVisitor visitor) => visitor.VisitSpaceSymbol(this);
+        public TResult Accept<TResult>(PGNTerminalSymbolVisitor<TResult> visitor) => visitor.VisitSpaceSymbol(this);
         public string GetText() => SpaceText;
     }
 
     public sealed class SideLineStartSymbol : PGNTerminalSymbol
     {
         public const string SideLineStartText = "(";
+
         public bool Equals(PGNTerminalSymbol other) => other is SideLineStartSymbol;
+        public void Accept(PGNTerminalSymbolVisitor visitor) => visitor.VisitSideLineStartSymbol(this);
+        public TResult Accept<TResult>(PGNTerminalSymbolVisitor<TResult> visitor) => visitor.VisitSideLineStartSymbol(this);
         public string GetText() => SideLineStartText;
     }
 
     public sealed class SideLineEndSymbol : PGNTerminalSymbol
     {
         public const string SideLineEndText = ")";
+
         public bool Equals(PGNTerminalSymbol other) => other is SideLineEndSymbol;
+        public void Accept(PGNTerminalSymbolVisitor visitor) => visitor.VisitSideLineEndSymbol(this);
+        public TResult Accept<TResult>(PGNTerminalSymbolVisitor<TResult> visitor) => visitor.VisitSideLineEndSymbol(this);
         public string GetText() => SideLineEndText;
     }
 
     public sealed class BlackToMoveEllipsisSymbol : PGNTerminalSymbol
     {
         public const string EllipsisText = "..";
+
         public bool Equals(PGNTerminalSymbol other) => other is BlackToMoveEllipsisSymbol;
+        public void Accept(PGNTerminalSymbolVisitor visitor) => visitor.VisitBlackToMoveEllipsisSymbol(this);
+        public TResult Accept<TResult>(PGNTerminalSymbolVisitor<TResult> visitor) => visitor.VisitBlackToMoveEllipsisSymbol(this);
         public string GetText() => EllipsisText;
     }
 
     public sealed class MoveCounterSymbol : PGNTerminalSymbol
     {
         readonly int value;
-        public bool Equals(PGNTerminalSymbol other) => other is MoveCounterSymbol && value == ((MoveCounterSymbol)other).value;
-        public string GetText() => value + ".";
         public MoveCounterSymbol(int value) { this.value = value; }
+
+        public bool Equals(PGNTerminalSymbol other) => other is MoveCounterSymbol && value == ((MoveCounterSymbol)other).value;
+        public void Accept(PGNTerminalSymbolVisitor visitor) => visitor.VisitMoveCounterSymbol(this);
+        public TResult Accept<TResult>(PGNTerminalSymbolVisitor<TResult> visitor) => visitor.VisitMoveCounterSymbol(this);
+        public string GetText() => value + ".";
     }
 
     public sealed class FormattedMoveSymbol : PGNTerminalSymbol
     {
         readonly string value;
-        public bool Equals(PGNTerminalSymbol other) => other is FormattedMoveSymbol && value == ((FormattedMoveSymbol)other).value;
-        public string GetText() => value;
         public readonly Chess.Variation Variation;
         public FormattedMoveSymbol(string value, Chess.Variation variation)
         {
             this.value = value;
             Variation = variation;
         }
+
+        public bool Equals(PGNTerminalSymbol other) => other is FormattedMoveSymbol && value == ((FormattedMoveSymbol)other).value;
+        public void Accept(PGNTerminalSymbolVisitor visitor) => visitor.VisitFormattedMoveSymbol(this);
+        public TResult Accept<TResult>(PGNTerminalSymbolVisitor<TResult> visitor) => visitor.VisitFormattedMoveSymbol(this);
+        public string GetText() => value;
     }
 }
