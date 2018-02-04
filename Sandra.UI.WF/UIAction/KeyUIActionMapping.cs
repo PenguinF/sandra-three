857557edb349d74245b5bd7a4c423ee305c99d2f
Henk Nicolai <hnicolai@thinkwisesoftware.com>
2018-02-04 17:57:29 +0100

    Deleted EnumerateEquivalentKeys() by assimilating it into IsMatch().

--- Sandra.UI.WF/UIAction/KeyUIActionMapping.cs
+++ Sandra.UI.WF/UIAction/KeyUIActionMapping.cs
@@ -108,12 +108,13 @@ namespace Sandra.UI.WF
         }
 
         /// <summary>
-        /// For a given shortcut key, enumerates all alternative shortcut keys which are generally considered equivalent.
+        /// Determines if a given shortcut matches a <see cref="ShortcutKeys"/> definition.
+        /// This takes into account alternative shortcut keys which are generally considered equivalent.
         /// An example is Ctrl+., where there are usually two keys that map to the '.' character.
         /// </summary>
-        public static IEnumerable<Keys> EnumerateEquivalentKeys(ShortcutKeys shortcutKeys)
+        public static bool IsMatch(ShortcutKeys shortcutKeys, Keys shortcut)
         {
-            if (shortcutKeys.IsEmpty) yield break;
+            if (shortcutKeys.IsEmpty) return false;
 
             Keys equivalentShortcut = (Keys)shortcutKeys.Key;
 
@@ -122,64 +123,55 @@ namespace Sandra.UI.WF
             if (code.HasFlag(KeyModifiers.Control)) equivalentShortcut |= Keys.Control;
             if (code.HasFlag(KeyModifiers.Alt)) equivalentShortcut |= Keys.Alt;
 
+            if (shortcut == equivalentShortcut) return true;
+
             Keys keyCode = equivalentShortcut & Keys.KeyCode;
-
-            yield return equivalentShortcut;
-
             Keys modifiers = equivalentShortcut & Keys.Modifiers;
             bool shift = equivalentShortcut.HasFlag(Keys.Shift);
+
             if (keyCode >= Keys.D0 && keyCode <= Keys.D9)
             {
-                yield return equivalentShortcut - Keys.D0 + Keys.NumPad0;
+                if (shortcut == equivalentShortcut - Keys.D0 + Keys.NumPad0) return true;
             }
             else if (keyCode >= Keys.NumPad0 && keyCode <= Keys.NumPad9)
             {
-                yield return equivalentShortcut - Keys.NumPad0 + Keys.D0;
+                if (shortcut == equivalentShortcut - Keys.NumPad0 + Keys.D0) return true;
             }
+
             else if (keyCode == Keys.Add)
             {
-                if (!shift) yield return modifiers | Keys.Shift | Keys.Oemplus;
+                if (!shift && shortcut == (modifiers | Keys.Shift | Keys.Oemplus)) return true;
             }
             else if (keyCode == Keys.Oemplus)
             {
-                if (shift) yield return modifiers | Keys.Add;
+                if (shift && shortcut == (modifiers | Keys.Add)) return true;
             }
+
             else if (keyCode == Keys.Subtract)
             {
-                yield return modifiers | Keys.OemMinus;
+                if (shortcut == (modifiers | Keys.OemMinus)) return true;
             }
             else if (keyCode == Keys.OemMinus)
             {
-                yield return modifiers | Keys.Subtract;
+                if (shortcut == (modifiers | Keys.Subtract)) return true;
             }
+
             else if (keyCode == Keys.Multiply)
             {
-                if (!shift) yield return modifiers | Keys.Shift | Keys.D8;
+                if (!shift && shortcut == (modifiers | Keys.Shift | Keys.D8)) return true;
             }
             else if (keyCode == Keys.D8)
             {
-                if (shift) yield return modifiers | Keys.Multiply;
+                if (shift && shortcut == (modifiers | Keys.Multiply)) return true;
             }
+
             else if (keyCode == Keys.Divide)
             {
-                yield return modifiers | Keys.OemQuestion;
+                if (shortcut == (modifiers | Keys.OemQuestion)) return true;
             }
             else if (keyCode == Keys.OemQuestion)
             {
-                yield return modifiers | Keys.Divide;
-            }
-        }
-
-        public static bool IsMatch(ShortcutKeys shortcutKeys, Keys shortcut)
-        {
-            foreach (var mappedShortcut in EnumerateEquivalentKeys(shortcutKeys))
-            {
-                // If the shortcut matches, then try to perform the action.
-                // If the handler does not return UIActionVisibility.Parent, then swallow the key by returning true.
-                if (mappedShortcut == shortcut)
-                {
-                    return true;
-                }
+                if (shortcut == (modifiers | Keys.Divide)) return true;
             }
 
             return false;
@@ -201,6 +193,8 @@ namespace Sandra.UI.WF
                 // Try to find an action with given shortcut.
                 foreach (var mapping in actionHandler.KeyMappings)
                 {
+                    // If the shortcut matches, then try to perform the action.
+                    // If the handler does not return UIActionVisibility.Parent, then swallow the key by returning true.
                     if (IsMatch(mapping.Shortcut, shortcut)
                         && actionHandler.TryPerformAction(mapping.Action, true).UIActionVisibility != UIActionVisibility.Parent)
                     {
@@ -208,6 +202,7 @@ namespace Sandra.UI.WF
                     }
                 }
             }
+
             return false;
         }
     }
