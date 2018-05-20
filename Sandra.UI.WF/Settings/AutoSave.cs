d42698ab82c56176963c22b30162ca34c97dea26
Henk Nicolai <hnicolai@thinkwisesoftware.com>
2018-05-20 16:10:10 +0200

    Added new class AutoSave.

---
+++ Sandra.UI.WF/Settings/AutoSave.cs
@@ -0,0 +1,28 @@
+﻿/*********************************************************************************
+ * AutoSave.cs
+ * 
+ * Copyright (c) 2004-2018 Henk Nicolai
+ * 
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ * 
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ * 
+ *********************************************************************************/
+namespace Sandra.UI.WF
+{
+    /// <summary>
+    /// Manages an auto-save file local to every non-roaming user.
+    /// See also: <seealso cref="System.Environment.SpecialFolder.LocalApplicationData"/>
+    /// </summary>
+    public sealed class AutoSave
+    {
+    }
+}
