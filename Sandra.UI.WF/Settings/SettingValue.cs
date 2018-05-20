9544dc2d5d4bb413ab251207c614923adf2812c0
Henk Nicolai <hnicolai@thinkwisesoftware.com>
2018-05-20 18:15:30 +0200

    Added visitor pattern.

--- Sandra.UI.WF/Settings/SettingValue.cs
+++ Sandra.UI.WF/Settings/SettingValue.cs
@@ -22,10 +22,36 @@ namespace Sandra.UI.WF
     /// Base interface for all types of values which are allowed in settings.
     /// </summary>
     /// <remarks>
-    /// Not using object because this impacts change tracking.
+    /// Not using <see cref="object"/> because this impacts change tracking.
     /// </remarks>
     public interface ISettingValue
     {
+        void Accept(SettingValueVisitor visitor);
+        TResult Accept<TResult>(SettingValueVisitor<TResult> visitor);
+    }
+
+    /// <summary>
+    /// Represents a visitor that visits a single <see cref="ISettingValue"/>.
+    /// See also: https://en.wikipedia.org/wiki/Visitor_pattern
+    /// </summary>
+    public abstract class SettingValueVisitor
+    {
+        public virtual void DefaultVisit(ISettingValue value) { }
+        public virtual void Visit(ISettingValue value) { if (value != null) value.Accept(this); }
+        public virtual void VisitBoolean(BooleanSettingValue value) => DefaultVisit(value);
+        public virtual void VisitInt32(Int32SettingValue value) => DefaultVisit(value);
+    }
+
+    /// <summary>
+    /// Represents a visitor that visits a single <see cref="ISettingValue"/>.
+    /// See also: https://en.wikipedia.org/wiki/Visitor_pattern
+    /// </summary>
+    public abstract class SettingValueVisitor<TResult>
+    {
+        public virtual TResult DefaultVisit(ISettingValue value) => default(TResult);
+        public virtual TResult Visit(ISettingValue value) => value == null ? default(TResult) : value.Accept(this);
+        public virtual TResult VisitBoolean(BooleanSettingValue value) => DefaultVisit(value);
+        public virtual TResult VisitInt32(Int32SettingValue value) => DefaultVisit(value);
     }
 
     /// <summary>
@@ -34,6 +60,9 @@ namespace Sandra.UI.WF
     public struct BooleanSettingValue : ISettingValue
     {
         public bool Value;
+
+        public void Accept(SettingValueVisitor visitor) => visitor.VisitBoolean(this);
+        public TResult Accept<TResult>(SettingValueVisitor<TResult> visitor) => visitor.VisitBoolean(this);
     }
 
     /// <summary>
@@ -42,5 +71,8 @@ namespace Sandra.UI.WF
     public struct Int32SettingValue : ISettingValue
     {
         public int Value;
+
+        public void Accept(SettingValueVisitor visitor) => visitor.VisitInt32(this);
+        public TResult Accept<TResult>(SettingValueVisitor<TResult> visitor) => visitor.VisitInt32(this);
     }
 }
