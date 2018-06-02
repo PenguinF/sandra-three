b152529617e9eacefe316f79344af5cc17ff9aa8
Henk Nicolai <hnicolai@thinkwisesoftware.com>
2018-06-02 19:38:02 +0200

    Move SettingReader and SettingWriter to their own files.

---
+++ Sandra.UI.WF/Settings/SettingReader.cs
@@ -0,0 +1,157 @@
+﻿/*********************************************************************************
+ * SettingReader.cs
+ * 
+ * Copyright (c) 2004-2018 Henk Nicolai
+ * 
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ * 
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ * 
+ *********************************************************************************/
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Numerics;
+
+namespace Sandra.UI.WF
+{
+    /// <summary>
+    /// Represents a single iteration of loading settings from a file.
+    /// </summary>
+    internal class SettingReader
+    {
+        private readonly JsonTextReader jsonTextReader;
+
+        public SettingReader(TextReader inputReader)
+        {
+            if (inputReader == null) throw new ArgumentNullException(nameof(inputReader));
+            jsonTextReader = new JsonTextReader(inputReader);
+        }
+
+        private Exception TokenTypeNotSupported(JsonToken jsonToken)
+            => new JsonReaderException($"Token type {jsonToken} is not supported for settings.");
+
+        private JsonToken ReadSkipComments()
+        {
+            // Skip comments until encountering something meaningful.
+            do jsonTextReader.Read(); while (jsonTextReader.TokenType == JsonToken.Comment);
+            return jsonTextReader.TokenType;
+        }
+
+        public bool TryParseValue(out PValue value)
+        {
+            var tokenType = ReadSkipComments();
+            if (tokenType == JsonToken.None)
+            {
+                value = default(PValue);
+                return false;
+            }
+
+            value = ParseValue(tokenType);
+
+            if (ReadSkipComments() != JsonToken.None)
+            {
+                throw new JsonReaderException("End of file expected");
+            }
+
+            return true;
+        }
+
+        private PMap ParseMap()
+        {
+            Dictionary<string, PValue> mapBuilder = new Dictionary<string, PValue>();
+
+            for (;;)
+            {
+                var tokenType = ReadSkipComments();
+                if (tokenType == JsonToken.EndObject)
+                {
+                    return new PMap(mapBuilder);
+                }
+
+                if (tokenType != JsonToken.PropertyName)
+                {
+                    throw new JsonReaderException("PropertyName or EndObject '}' expected");
+                }
+
+                string key = (string)jsonTextReader.Value;
+
+                // Expect unique keys.
+                if (mapBuilder.ContainsKey(key))
+                {
+                    throw new JsonReaderException($"Non-unique key in object: {key}");
+                }
+
+                mapBuilder.Add(key, ParseValue(ReadSkipComments()));
+            }
+        }
+
+        private PList ParseList()
+        {
+            List<PValue> listBuilder = new List<PValue>();
+
+            for (;;)
+            {
+                var tokenType = ReadSkipComments();
+                if (tokenType == JsonToken.EndArray)
+                {
+                    return new PList(listBuilder);
+                }
+
+                listBuilder.Add(ParseValue(tokenType));
+            }
+        }
+
+        private PValue ParseValue(JsonToken currentTokenType)
+        {
+            switch (currentTokenType)
+            {
+                case JsonToken.Boolean:
+                    return new PBoolean((bool)jsonTextReader.Value);
+
+                case JsonToken.Integer:
+                    return jsonTextReader.Value is BigInteger
+                        ? new PInteger((BigInteger)jsonTextReader.Value)
+                        : new PInteger((long)jsonTextReader.Value); ;
+
+                case JsonToken.String:
+                    return new PString((string)jsonTextReader.Value);
+
+                case JsonToken.StartObject:
+                    return ParseMap();
+
+                case JsonToken.StartArray:
+                    return ParseList();
+
+                case JsonToken.None:
+                    throw new JsonReaderException("Unexpected end of file");
+
+                case JsonToken.Comment:
+                case JsonToken.EndArray:
+                case JsonToken.EndObject:
+                case JsonToken.PropertyName:
+                    throw new JsonReaderException("'{', '[', Boolean, Integer or String expected");
+
+                case JsonToken.Bytes:
+                case JsonToken.Date:
+                case JsonToken.EndConstructor:
+                case JsonToken.Float:
+                case JsonToken.Null:
+                case JsonToken.Raw:
+                case JsonToken.StartConstructor:
+                case JsonToken.Undefined:
+                default:
+                    throw TokenTypeNotSupported(currentTokenType);
+            }
+        }
+    }
+}
---
+++ Sandra.UI.WF/Settings/SettingWriter.cs
@@ -0,0 +1,126 @@
+﻿/*********************************************************************************
+ * SettingWriter.cs
+ * 
+ * Copyright (c) 2004-2018 Henk Nicolai
+ * 
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ * 
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ * 
+ *********************************************************************************/
+using Newtonsoft.Json;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Sandra.UI.WF
+{
+    /// <summary>
+    /// Represents a single iteration of writing settings to a file.
+    /// </summary>
+    internal class SettingWriter : PValueVisitor
+    {
+        private readonly StringBuilder outputBuilder;
+        private readonly JsonTextWriter jsonTextWriter;
+
+        public SettingWriter()
+        {
+            outputBuilder = new StringBuilder();
+            jsonTextWriter = new JsonTextWriter(new StringWriter(outputBuilder));
+        }
+
+        public override void VisitBoolean(PBoolean value)
+        {
+            jsonTextWriter.WriteValue(value.Value);
+        }
+
+        public override void VisitInteger(PInteger value)
+        {
+            jsonTextWriter.WriteValue(value.Value);
+        }
+
+        public override void VisitList(PList value)
+        {
+            jsonTextWriter.WriteStartArray();
+            value.ForEach(Visit);
+            jsonTextWriter.WriteEndArray();
+        }
+
+        public override void VisitMap(PMap value)
+        {
+            jsonTextWriter.WriteStartObject();
+            foreach (var kv in value)
+            {
+                jsonTextWriter.WritePropertyName(kv.Key);
+                Visit(kv.Value);
+            }
+            jsonTextWriter.WriteEndObject();
+        }
+
+        public override void VisitString(PString value)
+        {
+            jsonTextWriter.WriteValue(value.Value);
+        }
+
+        public void WriteToFile(FileStream outputStream, Encoder encoder, char[] buffer, byte[] encodedBuffer)
+        {
+            jsonTextWriter.Close();
+            string output = outputBuilder.ToString();
+
+            // How much of the output still needs to be written.
+            int remainingLength = output.Length;
+
+            // Number of characters already written from output. Loop invariant therefore is:
+            // charactersCopied + remainingLength == output.Length.
+            int charactersCopied = 0;
+
+            // Fill up the character buffer before doing any writing.
+            for (;;)
+            {
+                // Determine number of characters to write.
+                // AutoSave.CharBufferSize is known to be equal to buffer.Length.
+                int charWriteCount = AutoSave.CharBufferSize;
+
+                // Remember if this fill up the entire buffer.
+                bool bufferFull = charWriteCount <= remainingLength;
+                if (!bufferFull) charWriteCount = remainingLength;
+
+                // Now copy to the character buffer after checking its range.
+                output.CopyTo(charactersCopied, buffer, 0, charWriteCount);
+
+                // If the buffer is full, call the encoder to convert it into bytes.
+                if (bufferFull)
+                {
+                    int bytes = encoder.GetBytes(buffer, 0, AutoSave.CharBufferSize, encodedBuffer, 0, false);
+                    outputStream.Write(encodedBuffer, 0, bytes);
+                }
+
+                // Update loop variables.
+                charactersCopied += charWriteCount;
+                remainingLength -= charWriteCount;
+
+                if (remainingLength == 0)
+                {
+                    // Process what's left in the buffer and Encoder.
+                    int bytes = encoder.GetBytes(buffer, 0, bufferFull ? 0 : charWriteCount, encodedBuffer, 0, true);
+                    if (bytes > 0)
+                    {
+                        outputStream.Write(encodedBuffer, 0, bytes);
+                    }
+
+                    // Make sure everything is written to the file.
+                    outputStream.Flush();
+                    return;
+                }
+            }
+        }
+    }
+}
