c658480331e87a4cbb3a68fbe8d485063915aa7c
Henk Nicolai <hnicolai@thinkwisesoftware.com>
2018-06-03 11:20:01 +0200

    Initialize SettingSchema with a set of properties.

--- Sandra.UI.WF/Settings/SettingSchema.cs
+++ Sandra.UI.WF/Settings/SettingSchema.cs
@@ -16,6 +16,45 @@
  *    limitations under the License.
  * 
  *********************************************************************************/
+using System.Collections.Generic;
+using System.Linq;
+
 namespace Sandra.UI.WF
 {
+    public class SettingSchema
+    {
+        // Prevent repeated allocations of empty dictionaries.
+        private static readonly Dictionary<string, SettingProperty> emptyProperties = new Dictionary<string, SettingProperty>();
+
+        /// <summary>
+        /// Represents the empty <see cref="SettingSchema"/>, which contains no properties.
+        /// </summary>
+        public static readonly SettingSchema Empty = new SettingSchema(null);
+
+        private readonly Dictionary<string, SettingProperty> properties;
+
+        /// <summary>
+        /// Initializes a new instance of a <see cref="SettingSchema"/>.
+        /// </summary>
+        /// <param name="properties">
+        /// The set of properties with unique keys to support.
+        /// </param>
+        /// <exception cref="System.ArgumentException">
+        /// Two or more properties have the same key.
+        /// </exception>
+        public SettingSchema(IEnumerable<SettingProperty> properties)
+        {
+            this.properties = properties != null && properties.Any()
+                ? new Dictionary<string, SettingProperty>()
+                : emptyProperties;
+
+            if (properties != null)
+            {
+                foreach (var property in properties)
+                {
+                    this.properties.Add(property.Name.Key, property);
+                }
+            }
+        }
+    }
 }
