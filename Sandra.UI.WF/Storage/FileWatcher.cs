192bc3fbd9b59c5c14f16ae6d8f13c4d77aefa60
Henk Nicolai <hnicolai@thinkwisesoftware.com>
2018-06-09 13:45:38 +0200

    First version of FileWatcher, which should recover from internal buffer overflow exceptions.

--- Sandra.UI.WF/Storage/FileWatcher.cs
+++ Sandra.UI.WF/Storage/FileWatcher.cs
@@ -16,9 +16,123 @@
  *    limitations under the License.
  * 
  *********************************************************************************/
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Threading;
+
 namespace Sandra.UI.WF.Storage
 {
-    internal class FileWatcher
+    internal class FileWatcher : IDisposable
     {
+        private readonly string filePath;
+
+        private FileSystemWatcher fileSystemWatcher;
+
+        private SynchronizationContext sc;
+
+        public FileWatcher(string filePath)
+        {
+            this.filePath = filePath;
+        }
+
+        private FileSystemWatcher createFileSystemWatcher()
+        {
+            var fileSystemWatcher = new FileSystemWatcher(Path.GetDirectoryName(filePath), Path.GetFileName(filePath));
+            fileSystemWatcher.Changed += watcher_ChangedCreatedDeleted;
+            fileSystemWatcher.Created += watcher_ChangedCreatedDeleted;
+            fileSystemWatcher.Deleted += watcher_ChangedCreatedDeleted;
+            fileSystemWatcher.Renamed += watcher_Renamed;
+            fileSystemWatcher.Error += watcher_Error;
+            return fileSystemWatcher;
+        }
+
+        /// <summary>
+        /// Make sure to only turn this on when the UI message loop is active.
+        /// </summary>
+        public bool EnableRaisingEvents
+        {
+            get { return fileSystemWatcher != null; }
+            set
+            {
+                bool enableRaisingEvents = EnableRaisingEvents;
+                if (enableRaisingEvents != value)
+                {
+                    if (value)
+                    {
+                        // Capture the synchronization context so events can be posted to it.
+                        sc = SynchronizationContext.Current;
+                        Debug.Assert(sc != null);
+
+                        fileSystemWatcher = createFileSystemWatcher();
+                        fileSystemWatcher.EnableRaisingEvents = true;
+                    }
+                    else
+                    {
+                        fileSystemWatcher.Dispose();
+                        fileSystemWatcher = null;
+                    }
+                }
+            }
+        }
+
+        public event Action FileChanged;
+
+        private enum FileChangeType
+        {
+            Change,
+            ErrorBufferOverflow,
+            ErrorUnspecified,
+        }
+
+        private void fileChangePosted(object state)
+        {
+            // Back on the UI thread, called from the background, so check if EnableRaisingEvents is still synchronized.
+            if (EnableRaisingEvents)
+            {
+                FileChangeType fileChangeType = (FileChangeType)state;
+
+                if (fileChangeType == FileChangeType.ErrorBufferOverflow)
+                {
+                    // Recreate the watcher, also raise the Changed event.
+                    fileSystemWatcher.Dispose();
+                    fileSystemWatcher = createFileSystemWatcher();
+                    fileSystemWatcher.EnableRaisingEvents = true;
+                }
+
+                if (fileChangeType != FileChangeType.ErrorUnspecified)
+                {
+                    FileChanged?.Invoke();
+                }
+            }
+        }
+
+        private void watcher_ChangedCreatedDeleted(object sender, FileSystemEventArgs e)
+            => sc.Post(fileChangePosted, FileChangeType.Change);
+
+        private void watcher_Renamed(object sender, RenamedEventArgs e)
+            => sc.Post(fileChangePosted, FileChangeType.Change);
+
+        private void watcher_Error(object sender, ErrorEventArgs e)
+        {
+            fileSystemWatcher.EnableRaisingEvents = false;
+            if (e.GetException() is InternalBufferOverflowException)
+            {
+                sc.Post(fileChangePosted, FileChangeType.ErrorBufferOverflow);
+            }
+            else
+            {
+                sc.Post(fileChangePosted, FileChangeType.ErrorUnspecified);
+            }
+        }
+
+        public void Dispose()
+        {
+            if (fileSystemWatcher != null)
+            {
+                fileSystemWatcher.Dispose();
+                fileSystemWatcher = null;
+            }
+        }
     }
 }
