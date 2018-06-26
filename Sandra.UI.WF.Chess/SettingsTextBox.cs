18af72b7fa24b849b6afd24a75759d1b35e55d93
Henk Nicolai <hnicolai@thinkwisesoftware.com>
2018-06-26 21:14:59 +0200

    Use errorStyle for JsonErrorString tokens in SettingsTextBox.

--- Sandra.UI.WF.Chess/SettingsTextBox.cs
+++ Sandra.UI.WF.Chess/SettingsTextBox.cs
@@ -84,6 +84,7 @@ namespace Sandra.UI.WF
             };
 
             public override TextElementStyle VisitComment(JsonComment symbol) => commentStyle;
+            public override TextElementStyle VisitErrorString(JsonErrorString symbol) => errorStyle;
             public override TextElementStyle VisitString(JsonString symbol) => stringStyle;
             public override TextElementStyle VisitUnknownSymbol(JsonUnknownSymbol symbol) => errorStyle;
             public override TextElementStyle VisitValue(JsonValue symbol) => valueStyle;
