71a206b21e4cdfe73a03b84c03b240e36efe154f
Henk Nicolai <hnicolai@thinkwisesoftware.com>
2018-07-07 17:13:24 +0200

    Add License #region.

--- Sandra.UI.WF.Chess/StandardChessBoardForm.UIActions.cs
+++ Sandra.UI.WF.Chess/StandardChessBoardForm.UIActions.cs
@@ -1,4 +1,5 @@
-﻿/*********************************************************************************
+﻿#region License
+/*********************************************************************************
  * StandardChessBoardForm.UIActions.cs
  * 
  * Copyright (c) 2004-2018 Henk Nicolai
@@ -16,6 +17,8 @@
  *    limitations under the License.
  * 
  *********************************************************************************/
+#endregion
+
 using System;
 using System.Drawing;
 using System.Windows.Forms;
