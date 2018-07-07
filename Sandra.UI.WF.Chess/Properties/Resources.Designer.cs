50660a6aab49f4df0d10fa719e263bcd5f9f8da1
Henk Nicolai <hnicolai@thinkwisesoftware.com>
2018-07-07 16:57:42 +0200

    Add 16x16 settings icon.

--- Sandra.UI.WF.Chess/Properties/Resources.Designer.cs
+++ Sandra.UI.WF.Chess/Properties/Resources.Designer.cs
@@ -79,5 +79,15 @@ namespace Sandra.UI.WF.Properties {
                 return ((System.Drawing.Icon)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap settings {
+            get {
+                object obj = ResourceManager.GetObject("settings", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
