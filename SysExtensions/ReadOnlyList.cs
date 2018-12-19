485cd140179ee2e1a7dd926ac24c3963e12c68f7
Henk Nicolai <hnicolai@thinkwisesoftware.com>
2018-12-19 23:56:16 +0100

    Add ReadOnlyList utility class which does not seem to have an equivalent in .NET Standard.

---
+++ SysExtensions/ReadOnlyList.cs
@@ -0,0 +1,83 @@
+﻿#region License
+/*********************************************************************************
+ * ReadOnlyList.cs
+ * 
+ * Copyright (c) 2004-2018 Henk Nicolai
+ * 
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ * 
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ * 
+ *********************************************************************************/
+#endregion
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SysExtensions
+{
+    /// <summary>
+    /// Represents a read-only list of elements that can be accessed by index.
+    /// </summary>
+    /// <typeparam name="T">
+    /// The type of elements in the read-only list.
+    /// </typeparam>
+    public class ReadOnlyList<T> : IReadOnlyList<T>
+    {
+        // Prevent repeated allocations of empty arrays.
+        private static readonly T[] emptyArray = new T[0];
+
+        private readonly T[] array;
+
+        /// <summary>
+        /// Initializes a new instance of <see cref="ReadOnlyList{T}"/>.
+        /// </summary>
+        /// <param name="elements">
+        /// The elements of the list.
+        /// </param>
+        public ReadOnlyList(IEnumerable<T> elements)
+            => array = elements == null || !elements.Any() ? emptyArray : elements.ToArray();
+
+        /// <summary>
+        /// Gets the element at the specified index in the read-only list.
+        /// </summary>
+        /// <param name="index">
+        /// The zero-based index of the element to get.
+        /// </param>
+        /// <returns>
+        /// The element at the specified index in the read-only list.
+        /// </returns>
+        /// <exception cref="IndexOutOfRangeException">
+        /// <paramref name="index"/>is less than 0 or greater than or equal to <see cref="Count"/>.
+        /// </exception>
+        public T this[int index] => array[index];
+
+        /// <summary>
+        /// Gets the number of elements in the list.
+        /// </summary>
+        public int Count => array.Length;
+
+        /// <summary>
+        /// Returns an enumerator that iterates through the list.
+        /// </summary>
+        /// <returns>
+        /// A <see cref="IEnumerator{T}"/> that can be used to iterate through the list.
+        /// </returns>
+        public IEnumerator<T> GetEnumerator()
+        {
+            foreach (T element in array) yield return element;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+}
