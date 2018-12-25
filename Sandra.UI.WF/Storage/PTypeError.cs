0d00e61f4184726542d15d6b22b82ed69bf14cb3
Henk Nicolai <hnicolai@thinkwisesoftware.com>
2018-12-25 22:53:28 +0100

    Add PTypeError subclass of JsonErrorInfo for type errors.

--- Sandra.UI.WF/Storage/PTypeError.cs
+++ Sandra.UI.WF/Storage/PTypeError.cs
@@ -19,6 +19,47 @@
  *********************************************************************************/
 #endregion
 
+using SysExtensions.Text.Json;
+using System;
+
 namespace Sandra.UI.WF.Storage
 {
+    /// <summary>
+    /// Represents an error caused by a value being of a different type than expected.
+    /// </summary>
+    public class PTypeError : JsonErrorInfo
+    {
+        /// <summary>
+        /// Gets the context insensitive information for this error message.
+        /// </summary>
+        public ITypeErrorBuilder TypeErrorBuilder { get; }
+
+        private PTypeError(ITypeErrorBuilder typeErrorBuilder, int start, int length)
+            : base(JsonErrorCode.Custom, start, length)
+        {
+            TypeErrorBuilder = typeErrorBuilder;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of <see cref="PTypeError"/>.
+        /// </summary>
+        /// <param name="typeErrorBuilder">
+        /// The context insensitive information for this error message.
+        /// </param>
+        /// <returns>
+        /// A <see cref="PTypeError"/> instance which generates a localized error message.
+        /// </returns>
+        /// <exception cref="ArgumentNullException">
+        /// <paramref name="typeErrorBuilder"/> is null.
+        /// </exception>
+        public static PTypeError Create(ITypeErrorBuilder typeErrorBuilder, int start, int length)
+        {
+            if (typeErrorBuilder == null) throw new ArgumentNullException(nameof(typeErrorBuilder));
+
+            return new PTypeError(
+                typeErrorBuilder,
+                start,
+                length);
+        }
+    }
 }
