039a73f5e55333132c6da4ce7bc4ede02c8f0f44
Henk Nicolai <hnicolai@thinkwisesoftware.com>
2019-03-04 21:43:40 +0100

    Remove modifiedBinding local variable. It is ignored while building the main menu.

--- Sandra.UI/MdiContainerForm.cs
+++ Sandra.UI/MdiContainerForm.cs
@@ -133,22 +133,14 @@ namespace Sandra.UI
             {
                 if (binding.DefaultInterfaces.TryGet(out IContextMenuUIActionInterface contextMenuInterface))
                 {
-                    // Copy the default binding and remove the ContextMenuUIActionInterface
-                    // which is used below to construct a UIMenuNode.Element directly.
-                    var modifiedBinding = new ImplementationSet<IUIActionInterface>();
-                    if (binding.DefaultInterfaces.TryGet(out IShortcutKeysUIActionInterface shortcutKeysInterface))
-                    {
-                        modifiedBinding.Add(shortcutKeysInterface);
-                    }
-
                     // Add a menu item inside the given container which will update itself after focus changes.
-                    container.Nodes.Add(new UIMenuNode.Element(binding.Action, shortcutKeysInterface, contextMenuInterface));
+                    container.Nodes.Add(new UIMenuNode.Element(binding.Action, binding.DefaultInterfaces.Get<IShortcutKeysUIActionInterface>(), contextMenuInterface));
 
                     // Register in a Dictionary to be able to figure out which menu items should be updated.
                     focusDependentUIActions.Add(binding.Action, new FocusDependentUIActionState());
 
                     // This also means that if a menu item is clicked, TryPerformAction() is called on the mainMenuActionHandler.
-                    mainMenuActionHandler.BindAction(new UIActionBinding(binding.Action, modifiedBinding, perform =>
+                    mainMenuActionHandler.BindAction(new UIActionBinding(binding, perform =>
                     {
                         try
                         {
