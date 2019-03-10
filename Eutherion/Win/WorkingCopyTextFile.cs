4d4fd36c3bbb08820054b93edca0f421acb04324
Henk Nicolai <hnicolai@thinkwisesoftware.com>
2019-03-10 16:34:18 +0100

    Add WorkingCopyTextFile.

---
+++ Eutherion/Win/WorkingCopyTextFile.cs
@@ -0,0 +1,32 @@
+﻿#region License
+/*********************************************************************************
+ * WorkingCopyTextFile.cs
+ *
+ * Copyright (c) 2004-2019 Henk Nicolai
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ *
+**********************************************************************************/
+#endregion
+
+namespace Eutherion.Win
+{
+    /// <summary>
+    /// Combines a <see cref="AutoSaveTextFile{TUpdate}"/> with a <see cref="LiveTextFile"/>
+    /// to create a working copy of a text file with local changes which persists across sessions,
+    /// and is aware of external updates to the text file on the file system.
+    /// </summary>
+    public sealed class WorkingCopyTextFile
+    {
+    }
+}
