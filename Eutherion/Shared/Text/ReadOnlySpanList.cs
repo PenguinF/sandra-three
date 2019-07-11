4cca654df04968ad246a1a4cd608efca2246d80c
Henk Nicolai <PenguinF@users.noreply.github.com>
2019-07-11 16:43:58 +0200

    Copy ReadOnlyList to new class ReadOnlySpanList, of which all elements must implement ISpan.

---
+++ Eutherion/Shared/Text/ReadOnlySpanList.cs
@@ -0,0 +1,97 @@
+﻿#region License
+/*********************************************************************************
+ * ReadOnlySpanList.cs
+ *
+ * Copyright (c) 2004-2019 Henk Nicolai
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ *
+**********************************************************************************/
+#endregion
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Eutherion.Text
+{
+    /// <summary>
+    /// Represents a read-only list of spanned elements that can be accessed by index.
+    /// </summary>
+    /// <typeparam name="TSpan">
+    /// The type of spanned elements in the read-only list.
+    /// </typeparam>
+    public class ReadOnlySpanList<TSpan> : IReadOnlyList<TSpan> where TSpan : ISpan
+    {
+        /// <summary>
+        /// Gets the empty <see cref="ReadOnlySpanList{T}"/>.
+        /// </summary>
+        public static readonly ReadOnlySpanList<TSpan> Empty = new ReadOnlySpanList<TSpan>(Array.Empty<TSpan>());
+
+        /// <summary>
+        /// Initializes a new instance of <see cref="ReadOnlySpanList{T}"/>.
+        /// </summary>
+        /// <param name="source">
+        /// The elements of the list.
+        /// </param>
+        /// <returns>
+        /// The initialized <see cref="ReadOnlySpanList{T}"/>.
+        /// </returns>
+        /// <exception cref="ArgumentNullException">
+        /// <paramref name="source"/> is null.
+        /// </exception>
+        public static ReadOnlySpanList<TSpan> Create(IEnumerable<TSpan> source)
+        {
+            if (source is ReadOnlySpanList<TSpan> readOnlyList) return readOnlyList;
+            var array = source.ToArrayEx();
+            return array.Length == 0 ? Empty : new ReadOnlySpanList<TSpan>(array);
+        }
+
+        private readonly TSpan[] array;
+
+        private ReadOnlySpanList(TSpan[] array) => this.array = array;
+
+        /// <summary>
+        /// Gets the spanned element at the specified index in the read-only list.
+        /// </summary>
+        /// <param name="index">
+        /// The zero-based index of the spanned element to get.
+        /// </param>
+        /// <returns>
+        /// The spanned element at the specified index in the read-only list.
+        /// </returns>
+        /// <exception cref="IndexOutOfRangeException">
+        /// <paramref name="index"/>is less than 0 or greater than or equal to <see cref="Count"/>.
+        /// </exception>
+        public TSpan this[int index] => array[index];
+
+        /// <summary>
+        /// Gets the number of spanned elements in the list.
+        /// </summary>
+        public int Count => array.Length;
+
+        /// <summary>
+        /// Returns an enumerator that iterates through the list.
+        /// </summary>
+        /// <returns>
+        /// A <see cref="IEnumerator{T}"/> that can be used to iterate through the list.
+        /// </returns>
+        public IEnumerator<TSpan> GetEnumerator()
+        {
+            foreach (TSpan element in array) yield return element;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+}
