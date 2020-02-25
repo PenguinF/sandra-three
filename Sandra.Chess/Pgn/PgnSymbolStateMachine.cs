7c4f5a2cfd00f93790ade67ef0be7b0f2723eeaf
Henk Nicolai <PenguinF@users.noreply.github.com>
2020-02-25 22:23:35 +0100

    Distinguish StatePiece from StateValidTagName.

--- Sandra.Chess/Pgn/PgnSymbolStateMachine.cs
+++ Sandra.Chess/Pgn/PgnSymbolStateMachine.cs
@@ -82,6 +82,7 @@ namespace Sandra.Chess.Pgn
         // Distinct states after the first character was a letter.
         private const int StateO = 15;
         private const int StateP = 16;
+        private const int StatePiece = 17;
 
         // StateO 'O' -> StateCastlingMove2 'O-' -> StateCastlingMove3 'O-O' -> StateCastlingMove4 'O-O-' -> StateCastlingMove5 'O-O-O'
         private const int StateCastlingMove2 = 18;
@@ -125,6 +126,7 @@ namespace Sandra.Chess.Pgn
         private const ulong ValidTagNameStates
             = 1ul << StateO
             | 1ul << StateP
+            | 1ul << StatePiece
             | 1ul << StateGotFile
             | 1ul << StatePawnCaptures
             | 1ul << StatePawnCapturesFile
@@ -174,18 +176,18 @@ namespace Sandra.Chess.Pgn
             // States when the first character is a letter.
             StateTransitionTable[StateStart, LetterO] = StateO;
             StateTransitionTable[StateStart, LetterP] = StateP;
+            StateTransitionTable[StateStart, OtherPieceLetter] = StatePiece;
             StateTransitionTable[StateStart, LowercaseAtoH] = StateGotFile;
 
             // Tag names must start with a letter or underscore.
             // This deviates from the PGN standard which only allows tag names to start with uppercase letters.
-            StateTransitionTable[StateStart, OtherPieceLetter] = StateValidTagName;
             StateTransitionTable[StateStart, OtherUpperCaseLetter] = StateValidTagName;
             StateTransitionTable[StateStart, LowercaseX] = StateValidTagName;
             StateTransitionTable[StateStart, OtherLowercaseLetter] = StateValidTagName;
 
             // Allow only digits, letters or the underscore character in tag names.
             // Some of the transitions here are overwritten below for specific move parsing.
-            new[] { StateO, StateP, StateGotFile,
+            new[] { StateO, StateP, StatePiece, StateGotFile,
                     StatePawnCaptures, StatePawnCapturesFile, StatePawnMoveComplete,
                     StateValidTagName }.ForEach(state =>
             {
