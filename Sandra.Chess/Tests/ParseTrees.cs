829a6cf95378bc8e43bd3b3172b1954b05c7a677
Henk Nicolai <PenguinF@users.noreply.github.com>
2020-05-04 18:22:02 +0200

    Add unit tests for tag sections.

--- Sandra.Chess/Tests/ParseTrees.cs
+++ Sandra.Chess/Tests/ParseTrees.cs
@@ -24,6 +24,7 @@ using Sandra.Chess.Pgn.Temp;
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Sandra.Chess.Tests
 {
@@ -81,6 +82,48 @@ namespace Sandra.Chess.Tests
         private static readonly ParseTree<PgnTriviaSyntax> WhitespaceThenCommentTrivia = new ParseTree<PgnTriviaSyntax> { WhitespaceThenComment, EmptyBackground };
         private static readonly ParseTree<PgnTriviaSyntax> WhitespaceCommentWhitespace = new ParseTree<PgnTriviaSyntax> { WhitespaceThenComment, Whitespace };
 
+        private static ParseTree<PgnSymbolWithTrivia> NoTrivia<TTagElement>()
+            where TTagElement : PgnSymbol
+            => new ParseTree<PgnSymbolWithTrivia> { EmptyTrivia, new ParseTree<TTagElement>() };
+
+        private static ParseTree<PgnSymbolWithTrivia> LeadingWhitespace<TTagElement>()
+            where TTagElement : PgnSymbol
+            => new ParseTree<PgnSymbolWithTrivia> { WhitespaceTrivia, new ParseTree<TTagElement>() };
+
+        private static readonly ParseTree<PgnSymbolWithTrivia> BracketClose = NoTrivia<PgnSymbol>();
+        private static readonly ParseTree<PgnSymbolWithTrivia> BracketOpen = NoTrivia<PgnSymbol>();
+        private static readonly ParseTree<PgnSymbolWithTrivia> ErrorTagValue = NoTrivia<PgnSymbol>();
+        private static readonly ParseTree<PgnSymbolWithTrivia> TagName = NoTrivia<PgnSymbol>();
+        private static readonly ParseTree<PgnSymbolWithTrivia> TagValue = NoTrivia<PgnSymbol>();
+
+        private static readonly ParseTree<PgnSymbolWithTrivia> WS_BracketClose = LeadingWhitespace<PgnSymbol>();
+        private static readonly ParseTree<PgnSymbolWithTrivia> WS_BracketOpen = LeadingWhitespace<PgnSymbol>();
+        private static readonly ParseTree<PgnSymbolWithTrivia> WS_ErrorTagValue = LeadingWhitespace<PgnSymbol>();
+        private static readonly ParseTree<PgnSymbolWithTrivia> WS_TagName = LeadingWhitespace<PgnSymbol>();
+        private static readonly ParseTree<PgnSymbolWithTrivia> WS_TagValue = LeadingWhitespace<PgnSymbol>();
+
+        private static List<ParseTree<PgnSymbolWithTrivia>> TagPair(ParseTree<PgnSymbolWithTrivia> firstSymbol, params ParseTree<PgnSymbolWithTrivia>[] otherSymbols)
+        {
+            var tagPairSyntax = new List<ParseTree<PgnSymbolWithTrivia>>() { firstSymbol };
+            otherSymbols.ForEach(tagPairSyntax.Add);
+            return tagPairSyntax;
+        }
+
+        private static ParseTree<PgnSyntaxNodes> TagSectionTrailingTrivia(
+            ParseTree<PgnTriviaSyntax> trailingTrivia,
+            List<ParseTree<PgnSymbolWithTrivia>> firstTagPair,
+            params List<ParseTree<PgnSymbolWithTrivia>>[] otherTagPairs)
+        {
+            var tagSectionSyntax = new ParseTree<PgnSyntaxNodes>();
+            firstTagPair.ForEach(tagSectionSyntax.Add);
+            otherTagPairs.ForEach(otherTagPair => otherTagPair.ForEach(tagSectionSyntax.Add));
+            tagSectionSyntax.Add(trailingTrivia);
+            return tagSectionSyntax;
+        }
+
+        private static ParseTree<PgnSyntaxNodes> TagSection(List<ParseTree<PgnSymbolWithTrivia>> firstTagPair, params List<ParseTree<PgnSymbolWithTrivia>>[] otherTagPairs)
+            => TagSectionTrailingTrivia(EmptyTrivia, firstTagPair, otherTagPairs);
+
         internal static readonly List<(string, ParseTree)> TestParseTrees = new List<(string, ParseTree)>
         {
             #region Background
@@ -124,6 +167,57 @@ namespace Sandra.Chess.Tests
             ("**{}*", new ParseTree<PgnSyntaxNodes> { SymbolNoTrivia, SymbolNoTrivia, PgnSymbolWithLeadingTrivia(CommentNoBackground, EmptyBackground), EmptyTrivia }),
 
             #endregion Combinations of trivia and symbols
+
+            #region Tag sections
+
+            ("[", TagSection(TagPair(BracketOpen))),
+            ("]", TagSection(TagPair(BracketClose))),
+            ("[]", TagSection(TagPair(BracketOpen, BracketClose))),
+            ("[[]", TagSection(TagPair(BracketOpen), TagPair(BracketOpen, BracketClose))),
+            ("[]]", TagSection(TagPair(BracketOpen, BracketClose), TagPair(BracketClose))),
+
+            // Missing brackets at 4 places.
+            // Last one doesn't miss brackets but still has a duplicate tag.
+            ("Event \"?\"\nEvent \"?\"", TagSection(TagPair(TagName, WS_TagValue), TagPair(WS_TagName, WS_TagValue))),
+            ("Event \"?\"\nEvent \"?\"]", TagSection(TagPair(TagName, WS_TagValue), TagPair(WS_TagName, WS_TagValue, BracketClose))),
+            ("Event \"?\"\n[Event \"?\"", TagSection(TagPair(TagName, WS_TagValue), TagPair(WS_BracketOpen, TagName, WS_TagValue))),
+            ("Event \"?\"\n[Event \"?\"]", TagSection(TagPair(TagName, WS_TagValue), TagPair(WS_BracketOpen, TagName, WS_TagValue, BracketClose))),
+            ("Event \"?\"]\nEvent \"?\"", TagSection(TagPair(TagName, WS_TagValue, BracketClose), TagPair(WS_TagName, WS_TagValue))),
+            ("Event \"?\"]\nEvent \"?\"]", TagSection(TagPair(TagName, WS_TagValue, BracketClose), TagPair(WS_TagName, WS_TagValue, BracketClose))),
+            ("Event \"?\"]\n[Event \"?\"", TagSection(TagPair(TagName, WS_TagValue, BracketClose), TagPair(WS_BracketOpen, TagName, WS_TagValue))),
+            ("Event \"?\"]\n[Event \"?\"]", TagSection(TagPair(TagName, WS_TagValue, BracketClose), TagPair(WS_BracketOpen, TagName, WS_TagValue, BracketClose))),
+            ("[Event \"?\"\nEvent \"?\"", TagSection(TagPair(BracketOpen, TagName, WS_TagValue), TagPair(WS_TagName, WS_TagValue))),
+            ("[Event \"?\"\nEvent \"?\"]", TagSection(TagPair(BracketOpen, TagName, WS_TagValue), TagPair(WS_TagName, WS_TagValue, BracketClose))),
+            ("[Event \"?\"\n[Event \"?\"", TagSection(TagPair(BracketOpen, TagName, WS_TagValue), TagPair(WS_BracketOpen, TagName, WS_TagValue))),
+            ("[Event \"?\"\n[Event \"?\"]", TagSection(TagPair(BracketOpen, TagName, WS_TagValue), TagPair(WS_BracketOpen, TagName, WS_TagValue, BracketClose))),
+            ("[Event \"?\"]\nEvent \"?\"", TagSection(TagPair(BracketOpen, TagName, WS_TagValue, BracketClose), TagPair(WS_TagName, WS_TagValue))),
+            ("[Event \"?\"]\nEvent \"?\"]", TagSection(TagPair(BracketOpen, TagName, WS_TagValue, BracketClose), TagPair(WS_TagName, WS_TagValue, BracketClose))),
+            ("[Event \"?\"]\n[Event \"?\"", TagSection(TagPair(BracketOpen, TagName, WS_TagValue, BracketClose), TagPair(WS_BracketOpen, TagName, WS_TagValue))),
+            ("[Event \"?\"]\n[Event \"?\"]", TagSection(TagPair(BracketOpen, TagName, WS_TagValue, BracketClose), TagPair(WS_BracketOpen, TagName, WS_TagValue, BracketClose))),
+
+            // Missing tag values.
+            ("Event\nEvent", TagSection(TagPair(TagName), TagPair(WS_TagName))),
+            ("\"?\"Event\nEvent", TagSection(TagPair(TagValue), TagPair(TagName), TagPair(WS_TagName))),
+            ("Event\"?\"\nEvent", TagSection(TagPair(TagName, TagValue), TagPair(WS_TagName))),
+            ("Event\n\"?\"Event", TagSection(TagPair(TagName, WS_TagValue), TagPair(TagName))),
+            ("Event\nEvent\"?\"", TagSection(TagPair(TagName), TagPair(WS_TagName, TagValue))),
+
+            // Duplicate values, missing tag names.
+            ("\"?\"\n\"?\"", TagSection(TagPair(TagValue, WS_TagValue))),
+            ("\"?\"\n]\"?\"", TagSection(TagPair(TagValue, WS_BracketClose), TagPair(TagValue))),
+            ("\"?\"[\n\"?\"", TagSection(TagPair(TagValue), TagPair(BracketOpen, WS_TagValue))),
+            ("\"?\"]\n[\"?\"", TagSection(TagPair(TagValue, BracketClose), TagPair(WS_BracketOpen, TagValue))),
+            ("\"?\"[\n]\"?\"", TagSection(TagPair(TagValue), TagPair(BracketOpen, WS_BracketClose), TagPair(TagValue))),
+            ("Event\"?\"\n\"?\"", TagSection(TagPair(TagName, TagValue, WS_TagValue))),
+            ("\"?\"\nEvent\"?\"", TagSection(TagPair(TagValue), TagPair(WS_TagName, TagValue))),
+            ("\"?\"\n\"?\"Event", TagSection(TagPair(TagValue, WS_TagValue), TagPair(TagName))),
+
+            // Whitespace between consecutive tag values.
+            ("\n\"?\"\n\"?\"", TagSection(TagPair(WS_TagValue, WS_TagValue))),
+            ("\"?\"\n\"?\"\n", TagSectionTrailingTrivia(WhitespaceTrivia, TagPair(TagValue, WS_TagValue))),
+            ("\n\"?\"\n\"?\"\n", TagSectionTrailingTrivia(WhitespaceTrivia, TagPair(WS_TagValue, WS_TagValue))),
+
+            #endregion Tag sections
         };
 
         internal static readonly List<(string, ParseTree, PgnErrorCode[])> TestParseTreesWithErrors = new List<(string, ParseTree, PgnErrorCode[])>
@@ -135,9 +229,9 @@ namespace Sandra.Chess.Tests
             (" % ", new ParseTree<PgnSyntaxNodes> { new ParseTree<PgnTriviaSyntax> { new ParseTree<PgnBackgroundListSyntax> { WhitespaceElement, IllegalCharacter, WhitespaceElement } } },
                 new[] { PgnErrorCode.IllegalCharacter }),
 
-            ("\"", new ParseTree<PgnSyntaxNodes> { SymbolNoTrivia, EmptyTrivia },
+            ("\"", TagSection(TagPair(ErrorTagValue)),
                 new[] { PgnErrorCode.UnterminatedTagValue }),
-            ("\"\n", new ParseTree<PgnSyntaxNodes> { SymbolNoTrivia, EmptyTrivia },
+            ("\"\n", TagSection(TagPair(ErrorTagValue)),
                 new[] { PgnErrorCode.IllegalControlCharacterInTagValue, PgnErrorCode.UnterminatedTagValue }),
 
             ("{", new ParseTree<PgnSyntaxNodes> { CommentTrivia }, new[] { PgnErrorCode.UnterminatedMultiLineComment }),
@@ -149,6 +243,16 @@ namespace Sandra.Chess.Tests
                 new[] { PgnErrorCode.IllegalCharacter, PgnErrorCode.IllegalCharacter }),
 
             #endregion Background and trivia errors
+
+            #region Tag sections
+
+            // Error tag values must behave like regular tag values.
+            ("[Event \"\\u\"]", TagSection(TagPair(BracketOpen, TagName, WS_ErrorTagValue, BracketClose)),
+                new[] { PgnErrorCode.UnrecognizedEscapeSequence }),
+            ("[Event \"\n\"]", TagSection(TagPair(BracketOpen, TagName, WS_ErrorTagValue, BracketClose)),
+                new[] { PgnErrorCode.IllegalControlCharacterInTagValue }),
+
+            #endregion Tag sections
         };
     }
 }
