db2ced15867a6aa539b54e7608c39769a95c7378
Henk Nicolai <PenguinF@users.noreply.github.com>
2020-06-11 20:39:17 +0200

    Emulate window menu in the globe menu dropdown.

--- Sandra.UI/MdiContainerForm.cs
+++ Sandra.UI/MdiContainerForm.cs
@@ -55,13 +55,20 @@ namespace Sandra.UI
             if (Session.Current.RegisteredLocalizers.Count() >= 2)
             {
                 // More than one localizer: can switch between them.
-                var langMenu = new List<Union<DefaultUIActionBinding, MainMenuDropDownItem>>();
-                langMenu.AddRange(Session.Current.RegisteredLocalizers.Select(x => (Union<DefaultUIActionBinding, MainMenuDropDownItem>)x.SwitchToLangUIActionBinding));
+                var langWindowMenu = new List<Union<DefaultUIActionBinding, MainMenuDropDownItem>>();
+                langWindowMenu.AddRange(Session.Current.RegisteredLocalizers.Select(x => (Union<DefaultUIActionBinding, MainMenuDropDownItem>)x.SwitchToLangUIActionBinding));
+
+                langWindowMenu.Add(SharedUIAction.WindowMenuRestore);
+                langWindowMenu.Add(SharedUIAction.WindowMenuMove);
+                langWindowMenu.Add(SharedUIAction.WindowMenuSize);
+                langWindowMenu.Add(SharedUIAction.WindowMenuMinimize);
+                langWindowMenu.Add(SharedUIAction.WindowMenuMaximize);
+                langWindowMenu.Add(SharedUIAction.Close);
 
                 mainMenuRootNodes.Add(new MainMenuDropDownItem
                 {
                     Container = new UIMenuNode.Container(null, SharedResources.globe.ToImageProvider()),
-                    DropDownItems = langMenu
+                    DropDownItems = langWindowMenu
                 });
             }
 
