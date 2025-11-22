76602c60d845ddf9be13062821948c10a66237ca
Henk Nicolai <PenguinF@users.noreply.github.com>
2025-11-22 19:49:20 +0100

    Introduce disposable DrawRun class.

---
+++ Eutherion/Win/Canvas/DrawRun.cs
@@ -0,0 +1,59 @@
+﻿#region License
+/*********************************************************************************
+ * DrawRun.cs
+ *
+ * Copyright (c) 2004-2025 Henk Nicolai
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ *
+**********************************************************************************/
+#endregion
+
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Eutherion.Win.Canvas
+{
+    /// <summary>
+    /// Represents a single invocation of a <see cref="Control"/> being drawn.
+    /// </summary>
+    public sealed class DrawRun : IDisposable
+    {
+        /// <summary>
+        /// Gets the graphics surface to draw on.
+        /// </summary>
+        public Graphics Graphics { get; }
+
+        /// <summary>
+        /// Initializes a new <see cref="DrawRun"/> with a Windows <see cref="System.Drawing.Graphics"/> object.
+        /// </summary>
+        /// <param name="graphics">
+        /// The Windows <see cref="System.Drawing.Graphics"/> object.
+        /// </param>
+        /// <exception cref="ArgumentNullException">
+        /// <paramref name="graphics"/> is <see langword="null"/>.
+        /// </exception>
+        public DrawRun(Graphics graphics)
+        {
+            Graphics = graphics ?? throw new ArgumentNullException(nameof(graphics));
+        }
+
+        public void Dispose()
+        {
+            GC.SuppressFinalize(this);
+        }
+
+        ~DrawRun() { }
+    }
+}
